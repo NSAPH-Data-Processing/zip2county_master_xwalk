fbf6355a3bc7a7a6e2c3ad22ea6b39801db80a47
James Kitch <jckitch31@gmail.com>
2024-05-28 10:24:49 -0400

    first pass of full crosswalk script

--- find_county_matches.py
+++ find_county_matches.py
@@ -2,21 +2,22 @@ import pandas as pd
 import argparse
 
 parser = argparse.ArgumentParser(description='Finds county match for given zip code')
-parser.add_argument('--zipcode', type=str, help='Zip code(s) of interest')
-parser.add_argument('--year', type=int, help='Year for crosswalk')
-parser.add_argument('--quarter', type=int, help='Quarter for crosswalk')
+parser.add_argument('--min_year', type=int, help='Minimum year for xwalk range, inclusive')
+parser.add_argument('--max_year', type=int, help='Maximium year for xwalk range, inclusive')
 parser.add_argument('--wd', type=str, help='Working dir for pipeline')
+parser.add_argument('--criteria', 
+                    type=str, default="total_ratio", 
+                    help='Method to determine crosswalk groupings by')
 
 args = parser.parse_args()
-zip_codes = args.zipcode.split(",")
-year = args.year
-quarter = args.quarter
+min_year = args.min_year
+max_year = args.max_year
 wd = args.wd
+criteria = args.criteria
 
 # input/output file setup
-#wd = "/Users/jck019/desktop/nsaph/data_team/zip-fips-crosswalk"
-infile = wd + "/data/intermediate/zip2fips_clean_{quarter}{year}.csv"
-outfile = wd + "/data/output/zip2fips_matches_{quarter}{year}_{criteria}.csv"
+infile = wd + "/data/intermediate/zip2fips_xwalk_clean.csv"
+outfile = wd + "/data/output/zip2fips_master_xwalk_{min_year}_{max_year}_{criteria}.csv"
 
 # full criteria list ["res_ratio", "bus_ratio", "oth_ratio", "tot_ratio"]
 
@@ -30,23 +31,20 @@ dtype_dict = {
 }
 
 # tbd on data type here. 
-def find_match(zip_lst, quarter, year, criteria = "tot_ratio"):
+def find_match(fname_in, fname_out, criteria):
     # load xwalk
-    df = pd.read_csv(infile.format(quarter = str(quarter), year = str(year)),
-                     dtype=dtype_dict)
+    df = pd.read_csv(fname_in, dtype=dtype_dict)
     
-    # filter df for zip codes in list
-    df = df[df["zip"].isin(zip_lst)]
-    idxs = df.groupby("zip")[criteria].idxmax()
-    df_match = df.loc[idxs, ["zip", "fips", criteria]]
-    df_match["year"] = year
-    df_match["quarter"] = quarter
+    # grouping df
+    idxs = df.groupby(["zip", "year", "quarter"])[criteria].idxmax()
+    df_match = df.loc[idxs, ["zip", "fips", "year", "quarter", criteria]]
 
     # writing matches to csv
-    df_match.to_csv(outfile.format(quarter=str(quarter), 
-                                    year = str(year), 
-                                    criteria = str(criteria)),
-                    index=False)
+    df_match.to_csv(fname_out, index=False)
     #return(df_match)
-    
-find_match(zip_lst=zip_codes, quarter=quarter, year=year)
+
+find_match(fname_in = infile,
+           fname_out = outfile.format(min_year=str(min_year), 
+                                        max_year = str(max_year), 
+                                        criteria = criteria),
+            criteria=criteria)
--- run_xwalk_pipeline.sh
+++ run_xwalk_pipeline.sh
@@ -5,9 +5,12 @@
 
 WD="/Users/jck019/desktop/nsaph/data_team/zip-fips-crosswalk"
 quarter=1
-year=2018
-zipcode="02478,02138,39564,43081"
+min_year=2010
+max_year=2020
+criteria="tot_ratio"
+#year=2018
+#zipcode="02478,02138,39564,43081"
 
-python3 $WD/download_hud_xwalk.py --year $year --quarter $quarter --wd $WD
-python3 $WD/clean_hud_xwalk.py --year $year --quarter $quarter --wd $WD
-python3 $WD/find_county_matches.py --zipcode $zipcode --year $year --quarter $quarter --wd $WD
+python3 $WD/download_hud_xwalk.py --min_year $min_year --max_year $max_year --quarter $quarter --wd $WD
+python3 $WD/clean_hud_xwalk.py --min_year $min_year --max_year $max_year --quarter $quarter --wd $WD
+python3 $WD/find_county_matches.py --min_year $min_year --max_year $max_year --wd $WD --criteria $criteria
