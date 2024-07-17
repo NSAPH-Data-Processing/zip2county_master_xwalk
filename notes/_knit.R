430bc925e6b8e9e1ac1cde29f148578b88126363
James Kitch <jckitch31@gmail.com>
2024-07-17 15:18:07 -0400

    Update to P.O. box notes file

--- notes/_knit.R
+++ notes/_knit.R
@@ -1,7 +1,7 @@
 ## Set the working directory as the location of the _knit.R file (default rmarkdown and Rmd configurations)
 
 ## knit ----
-rmarkdown::render("./notes.Rmd", 
+rmarkdown::render("./notes/notes.Rmd", 
                   output_dir = "./_knit")
 
 md_filename <- "./_knit/notes.md"
@@ -11,7 +11,7 @@ cat(md_txt, file=md_filename, sep="\n")
 
 
 
-rmarkdown::render("./notes_pobox.Rmd", 
+rmarkdown::render("./notes/notes_pobox.Rmd", 
                   output_dir = "./_knit")
 
 md_filename <- "./_knit/notes_pobox.md"
